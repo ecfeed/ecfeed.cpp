58b422d691232982d649f15cc8e1243dd9062609
krzysztof skorupski <k.skorupski@testify.no>
2020-10-06 09:04:08 +0200

    update

--- src/test.cpp
+++ src/test.cpp
@@ -1,39 +1,56 @@
 #include "main.cpp"
 #include "gtest/gtest.h"
 #include <iostream>
+#include<queue>
 
 class myTestFixture1: public ::testing::Test { 
 public: 
-  ecfeed::TestProvider* testProvider;
+    ecfeed::TestProvider* testProvider;
 
-   void SetUp(git status) override { 
-      testProvider = new ecfeed::TestProvider(
-        "ZCPH-DFYI-R7R7-R6MM-89L8", 
-        "/home/krzysztof/Desktop/git/ecfeed.java/com.ecfeed.runner/src/test/resources/security.p12", 
-        "develop-gen.ecfeed.com");
-    }
+    void SetUp() override { 
+        testProvider = new ecfeed::TestProvider(
+            "ZCPH-DFYI-R7R7-R6MM-89L8", 
+            "/home/krzysztof/Desktop/git/ecfeed.java/com.ecfeed.runner/src/test/resources/security.p12", 
+            "develop-gen.ecfeed.com");
+        }
 
     void TearDown() override {
-      delete testProvider;
+        delete testProvider;
     }
 };
 
 struct TestParameter{
-  int a;
-  bool b;
+    int a;
+    bool b;
 
-  TestParameter(int a_, bool b_) : a(a_), b(b_) { }
+    TestParameter(int a_, bool b_) : a(a_), b(b_) { }
 };
 
 std::ostream& operator<<(std::ostream& os, const TestParameter& p) {
-  return os << p.a << ", " << p.b; 
+    return os << p.a << ", " << p.b; 
 }
 
-class ParameterizedEcFeedProviderFixture : public ::testing::TestWithParam<TestParameter> {};
-std::vector<TestParameter> test_parameters({TestParameter(1, true), TestParameter(2, false)});
+class ParameterizedEcFeedProviderFixture : public ::myTestFixture1, public ::testing::WithParamInterface<std::string> {};
+std::vector<TestParameter> test_parameters_1({TestParameter(1, true), TestParameter(2, false)});
 
-INSTANTIATE_TEST_CASE_P(testSuite, ParameterizedEcFeedProviderFixture, ::testing::ValuesIn(test_parameters));
+std::vector<std::string> test_parameters_2() {
+    ecfeed::TestProvider tp(
+        "ZCPH-DFYI-R7R7-R6MM-89L8", 
+        "/home/krzysztof/Desktop/git/ecfeed.java/com.ecfeed.runner/src/test/resources/security.p12", 
+        "develop-gen.ecfeed.com"
+    );
+
+    std::vector<std::string> data;
+    for (std::string element : *tp.exportNwise("QuickStart.test", ecfeed::TemplateType::CSV)) {
+        data.push_back(element);
+    }
+
+    return data;
+}
+
+
+INSTANTIATE_TEST_CASE_P(testSuite, ParameterizedEcFeedProviderFixture, ::testing::ValuesIn(test_parameters_2()));
 TEST_P(ParameterizedEcFeedProviderFixture, testProvider1) {
 
-  std::cout << GetParam() << std::endl;
+   std::cout << GetParam() << std::endl;
 }
