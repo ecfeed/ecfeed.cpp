dd6c05d5d5d4e6363550a6f2d7bb6355482bc6d8
Krzysztof Skorupski <krzysztof.skorupski@scattering.eu>
2020-10-22 11:02:00 +0200

    cleanup

--- test/plain/main.cpp
+++ test/plain/main.cpp
@@ -3,8 +3,6 @@
 #include "nwise_export.h"
 #include <iostream>
 
-// clear && g++ test/multiple_headers/main.cpp test/multiple_headers/nwise_generate.cpp test/multiple_headers/nwise_export.cpp -o output -std=c++17 -pthread -lcurl -lcrypto -lstdc++fs && ./output
-
 int main() {
     
     std::string model = "V0G6-MHNQ-PDSR-G2WB-XOKV";
