895156b6b0a181d82e309087bcec303d9bece0bd
krzysztof skorupski <k.skorupski@testify.no>
2021-07-06 13:55:56 +0200

    test plain

--- test/plain/nwise_export.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <iostream>
-#include <vector>
-
-std::vector<std::string> nwise_export(std::string model, std::string method);
--- test/plain/nwise_generate.cpp
+++
@@ -1,7 +0,0 @@
-#include "nwise_generate.h"
-
-std::vector<ecfeed::test_arguments> nwise_generate(std::string model, std::string method) {
-    ecfeed::test_provider testProvider(model);
-
-    return testProvider.generate_pairwise(method)->to_list();
-}
