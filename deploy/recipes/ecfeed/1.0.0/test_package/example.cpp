a90e4b6f41ea4a786f0641047a93886047f02be6
krzysztof skorupski <k.skorupski@testify.no>
2021-07-07 13:09:20 +0200

    conan center deploy

--- deploy/recipes/ecfeed/1.0.0/test_package/example.cpp
+++
@@ -1,7 +0,0 @@
-#include "ecfeed.hpp"
-#include <iostream>
-
-int main() {
-    ecfeed::template_type type = ecfeed::template_type::csv;
-    std::cout << ecfeed::template_type_url_param(type) << std::endl;
-}
--- deploy/recipes/ecfeed/1.1.0/conanfile.py
+++ deploy/recipes/ecfeed/1.1.0/conanfile.py
@@ -1,21 +1,31 @@
 from conans import ConanFile, tools
 
+required_conan_version = ">=1.33.0"
+
 class EcFeedConan(ConanFile):
    name = "ecfeed"
    version = "1.1.0"
    license = "EPL-1.0"
-   url = "https://github.com/ecfeed/ecfeed.cpp"
-   author = "ecFeed AS (mail@ecfeed.com)"
+   url = "https://github.com/conan-io/conan-center-index"
+   homepage = "https://ecfeed.com/"
    description = "An open library used to connect to the ecFeed service. It can be integrated with the most common testing frameworks and generates a stream of test cases using a selected algorithm (e.g. Cartesian, N-Wise)."
    topics = ("test", "ecfeed", "nwise")
    build_policy = 'missing'
    requires = "libcurl/7.72.0", "openssl/1.1.1c"
+   no_copy_source = True
+   # url = "https://github.com/ecfeed/ecfeed.cpp"
+   # author = "ecFeed AS (mail@ecfeed.com)"
+
+   @property
+   def _source_subfolder(self):
+      return "source_subfolder"
 
    def source(self):
       tools.get(**self.conan_data["sources"][self.version])
 
    def package(self):
       self.copy("*.hpp", dst="include", keep_path=False)
+      self.copy("*license.txt", dst="licenses", keep_path=False)
 
    def package_id(self):
       self.info.header_only()
